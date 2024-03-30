af36d7a8ec3678b1cba0f175e652b7ff34ce8594
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-03-30 10:32:53 +0530

    feat: implemented tie case handler and refactored the code to be efficient on gas by removing loops

--- contract/ballot_block.sol
+++ contract/ballot_block.sol
@@ -10,24 +10,26 @@ pragma solidity >=0.7.0<0.9.0;
 
 
 contract ballot_block{
+
     bool isVotingStarted;
     bool isVotingEnded;
+    address administrator;
     bool isTied;
+    struct pair {
+        bool voted;
+        uint8 round;
+    }
     struct vote{
         address candidateAddress;
         uint256 timeStamp;
     }
-
-    mapping(address => uint) public votesForCandidates;
-    mapping(address => bool) public hasVoted;
-
-    address[4] private candidates = [
-        0xdD870fA1b7C4700F2BD7f44238821C26f7392148,
-        0x583031D1113aD414F02576BD6afaBfb302140225,
-        0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB,
-        0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
-    ];
-
+    mapping(address => uint8) votesForCandidates;
+    mapping(address => pair) hasVoted;
+    mapping(address => bool) validCandidates;
+    uint8 currentVotingRound;
+    uint8 winningVoteCount;
+    address[] candidates;
+    address winner;
     event startingVote(address startedby);
     event endingVote(address endedby);
     event AddVote(address indexed voter, address receiver);
@@ -37,67 +39,124 @@ contract ballot_block{
         _;
     }
 
+    modifier onlyChairperson() {
+        require(msg.sender == administrator, "Only administrator can call this function");
+        _;
+    }
+    modifier votingOpen() {
+        require(isVotingStarted, "Voting is not started yet");
+        _;
+    }
+    modifier votingNotStarted_OR_tied(){
+        require(!isVotingStarted || isTied, "Voting is started already");
+        _;
+    }
+    modifier notTied(){
+        require(!isTied, "Voting is Ended up in Tie, Wait for the administrator to start the next round election...");
+        _;
+    }
+    modifier votingNotClosed(){
+        require(!isVotingEnded, "Voting is Closed");
+        _;
+    }
+    modifier hasNotVoted() {
+        require(!hasVoted[msg.sender].voted || (hasVoted[msg.sender].round != currentVotingRound), "You have already voted");
+        _;
+    }
+    modifier isValidCandidate(address candidate) {
+        require(validCandidates[candidate], "Invalid candidate");
+        _;
+    }
     constructor()
     {
+        administrator = msg.sender;
         isVotingStarted=false;
         isVotingEnded=false;
         isTied=false;
-        candidates = [0xdD870fA1b7C4700F2BD7f44238821C26f7392148, 0x583031D1113aD414F02576BD6afaBfb302140225];
+        currentVotingRound = 0;
+        candidates.push(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+        candidates.push(0x583031D1113aD414F02576BD6afaBfb302140225);
+        candidates.push(0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB);
+        candidates.push(0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C);
+
+        validCandidates[0xdD870fA1b7C4700F2BD7f44238821C26f7392148] = true;
+        validCandidates[0x583031D1113aD414F02576BD6afaBfb302140225] = true;
+        validCandidates[0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB] = true;
+        validCandidates[0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C] = true;
+
     }
 
-    function Start() external returns (bool)
+    function Start() external onlyChairperson votingNotStarted_OR_tied returns (bool)
     {
-
-        require(!isVotingStarted, "Voting has already started");
+        currentVotingRound++;
+        winningVoteCount = 0;
         isVotingStarted = true;
+        isVotingEnded = false;
+        isTied = false;
         emit startingVote(msg.sender);
         return true;
 
     }
 
-    function End() external returns(bool)
+    function End() external onlyChairperson votingOpen votingNotClosed returns(bool)
     {
-        //write your code here
-        require(isVotingStarted, "Voting has not started yet");
-        require(!isVotingEnded, "Voting has already ended");
         isVotingEnded = true;
         isVotingStarted = false;
         emit endingVote(msg.sender);
         return true;
     }
 
-    function Add(address receiver) external returns(bool)
-    {
-        // write your code here
-        require(isVotingStarted, "Voting has not started");
-        require(!isVotingEnded, "Voting has already ended");
-        require(!hasVoted[msg.sender], "You have already voted");
-        require(isValidCandidate(receiver), "Not a valid candidate");
-
-        votesForCandidates[receiver] += 1;
-        hasVoted[msg.sender] = true;
-        emit AddVote(msg.sender, receiver);
-        return true;
-    }
-
-    function result() external view onlyAfterVoteEnd returns (address winningCandidate, uint highestVotes) {
-        highestVotes = 0;
-        for (uint i = 0; i < candidates.length; i++) {
-            if (votesForCandidates[candidates[i]] > highestVotes) {
-                winningCandidate = candidates[i];
-                highestVotes = votesForCandidates[candidates[i]];
-            }
+    function Add(address candidate) external votingOpen votingNotClosed hasNotVoted isValidCandidate(candidate) {
+        hasVoted[msg.sender].voted = true;
+        hasVoted[msg.sender].round = currentVotingRound;
+        votesForCandidates[candidate]++;
+        emit AddVote(msg.sender, candidate);
+        if (votesForCandidates[candidate] > winningVoteCount) {
+            winningVoteCount = votesForCandidates[candidate];
+            winner = candidate;
+        } else if (votesForCandidates[candidate] == winningVoteCount) {
+            winner = address(0);
         }
     }
 
-    function isValidCandidate(address candidate) internal view returns (bool) {
-        for(uint i = 0; i < candidates.length; i++) {
-            if (candidates[i] == candidate) {
-                return true;
+
+    function result() external onlyChairperson onlyAfterVoteEnd{
+        if (winner == address(0)){
+            if (votesForCandidates[candidates[0]] != winningVoteCount){
+                validCandidates[candidates[0]] = false;
             }
+            else{
+                votesForCandidates[candidates[0]] = 0;
+            }
+            if (votesForCandidates[candidates[1]] != winningVoteCount){
+                validCandidates[candidates[1]] = false;
+            }
+            else{
+                votesForCandidates[candidates[1]] = 0;
+            }
+            if (votesForCandidates[candidates[2]] != winningVoteCount){
+                validCandidates[candidates[2]] = false;
+            }
+            else{
+                votesForCandidates[candidates[2]] = 0;
+            }
+            if (votesForCandidates[candidates[3]] != winningVoteCount){
+                validCandidates[candidates[3]] = false;
+            }
+            else{
+                votesForCandidates[candidates[3]] = 0;
+            }
+            isTied = true;
+            return;
         }
-        return false;
+        isVotingStarted = false;
+        return;
     }
+
+    function getWinner() external view onlyAfterVoteEnd notTied returns (address){
+        return winner;
+    }
+
 
     
 
