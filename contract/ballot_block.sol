0cb446b697eba83f1b0d0b058cbb85f5d4d2c50d
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2024-04-09 23:17:26 +0530

    feat: test

--- contract/ballot_block.sol
+++ contract/ballot_block.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0<0.9.0;
 
-
+//Candidates: 
 //1st candidate : 0xdD870fA1b7C4700F2BD7f44238821C26f7392148
 //2nd candidate : 0x583031D1113aD414F02576BD6afaBfb302140225
 //3rd candidate : 0x4B0897b0513fdC7C541B6d9D7E929C4e5364D2dB
